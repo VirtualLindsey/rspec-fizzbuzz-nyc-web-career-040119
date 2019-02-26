cb4d3042cd8cde80ac9ab9fe19bcf74bc569cd67
L D <tapircode@gmail.com>
2019-02-26 00:11:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,11 +5,8 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
-  end 
   elsif num % 3 == 0
     return "Fizz"
-  end
   elsif num % 5 == 0
     return "Buzz"
-  end
 end
